d11d4e86ecea7b4af291d1d66871a85e0fd1cfda
hewigovens <360470+hewigovens@users.noreply.github.com>
2025-07-23 21:21:02 +0900

    Initial Commit

---
+++ Miso/MisoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisoApp.swift
+//  Miso
+//
+//  Created by hewig on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MisoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
